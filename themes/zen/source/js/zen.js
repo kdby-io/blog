690609a1c94cf1418c4e387f02a5a0db1947618d
Sangwon Kim <dev@pueue.com>
2019-01-18 07:44:42 +0900

    feat(wheelspin): English

--- themes/zen/source/js/zen.js
+++ themes/zen/source/js/zen.js
@@ -1,12 +1,12 @@
 const list = [
-  "시간을 죽이고 전기를 낭비하는 손쉬운 방법",
-  "혼자 구상하고 혼자 만들어서 혼자 낄낄거리는 개발자",
-  "지갑 사정에 전혀 도움되지 않는 것들만 만듭니다",
-  "음주코딩 전문가",
-  "게으르게 살기 위해 열심히 일하는 개발자",
-  "그래서 이걸로 뭘 할 수 있는데?",
-  "버림받은 아이디어들을 잘도 주워 먹습니다",
-  "엔트로피를 만드는 개발자",
+  "Killing time and wasting electricity",
+  "Designing alone, producing alone and giggling alone",
+  "Creating the unremunerative",
+  "Expert in drunken programming",
+  "Working hard for living lazy",
+  "So, what is that for?",
+  "Picking abandoned ideas off the ground",
+  "Entropy increaser",
 ];
 
 const spinSpinWheelspin = () => {
